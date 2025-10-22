76de58c0f03d93fae5aadc7cba50804ad05b3e3e
Rntrocha <renatosousa0706@gmail.com>
2025-10-22 15:37:26 +0000

    commit movimento cavalo

--- xadrez.c
+++ xadrez.c
@@ -7,6 +7,7 @@ int main()
     int bispo = 1;
     int torre = 1;
     int rainha = 1;
+    int cavalo = 1;
 
     printf("MOVENDO A PEÇA BISPO: \n"); // imprimindo informação do que irei realizar
 
@@ -34,6 +35,38 @@ int main()
         rainha++;
     } while (rainha <= 7);
     printf("Rainha para esquerda! \n"); // imprimindo o Ssegundo movimento da rainha
+
+    int escolha;
+    printf("Agora é a vez de mover o Cavalo !\n");
+    printf("Diga qual movimento você quer :\n");
+    printf("1.Direita:\n");
+    printf("2.Esquerda:\n");
+    printf("3.Cima:\n");
+    printf("Digite 1, 2 ou 3\n");
+    scanf("%d", &escolha);
+    // Movendo a peça cavalo em L
+    switch (escolha)
+    {
+    case 1:
+        printf("O cavalo não pode se mover para direita! \n");
+        break;
+    case 2:
+        printf("O cavalo não pode se mover para esquerda! \n");
+        break;
+    case 3:
+        while (cavalo--) // decrementada (--) para executar somente uma vez, código externo
+        {
+            for (int i = 0; i < 2; i++) // excutando dois movimentos para cima
+            {
+                printf("Cavalo para Cima ! \n"); // imprime 'cima' duas vezes
+            }
+            printf("Cavalo para direita! \n"); // imprime 'direita' uma vez
+        }
+
+    default:
+        printf("Opção inválida, digite 1,2 ou 3 para selecionar o movimento !!! \n");
+        break;
+    }
 
     return 0;
 }
